c1df592544ff6fb808cec4ea19e69d9ae78683ce
崔炎新 <yanxin.cui@nanhua.com.cn>
2015-09-08 17:36:38 +0800

    150908 17:36

--- src/com/nanhua/nhpims/DelRegActivity.java
+++ src/com/nanhua/nhpims/DelRegActivity.java
@@ -3,25 +3,28 @@ package com.nanhua.nhpims;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.widget.*;
 
 public class DelRegActivity extends Activity {
 
 	private static final String MOD_NAME = "发货登记";
-	EditText etProNum;
-	Button btnScan;
-	Button btnAdd;
-	Button btnClr;
-	ListView lvProToDel; // 待发货的产品
-	List<HashMap<String, Object>> data;
-	SimpleAdapter adapter;
+	private EditText etProNum;
+	private Button btnScan;
+	private Button btnAdd;
+	private Button btnClr;
+	private ListView lvProToDel; // 待发货的产品
+	private List<HashMap<String, Object>> data;
+	private MyAdapter adapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -41,14 +44,14 @@ public class DelRegActivity extends Activity {
 		String[] from = new String[] { "ibDel", "tvProNum", "tvProModel" };
 		int[] to = new int[] { R.id.ibDel, R.id.tvProNum, R.id.tvProModel };
 		// 创建SimpleAdapter适配器将数据绑定到item显示控件上
-		adapter = new SimpleAdapter(this, data, R.layout.del_item, from, to);
+		adapter = new MyAdapter();
 		// 实现列表的显示
 		lvProToDel.setAdapter(adapter);
 		// //条目点击事件
 		// listView.setOnItemClickListener(new ItemClickListener());
 	}
 
-	class ClickEvent implements OnClickListener {
+	private class ClickEvent implements OnClickListener {
 
 		@Override
 		public void onClick(View v) {
@@ -65,13 +68,13 @@ public class DelRegActivity extends Activity {
 		}
 	}
 
-	void onClick_Scan() {
+	private void onClick_Scan() {
 		// 打开扫描界面扫描条形码或二维码
 		Intent openCameraIntent = new Intent(this, CaptureActivity.class);
 		startActivityForResult(openCameraIntent, 0);
 	}
 
-	void onClick_Add() {
+	private void onClick_Add() {
 		String proNum = etProNum.getText().toString();
 		proNum.trim();
 		if (proNum.equals("")) {
@@ -143,6 +146,77 @@ public class DelRegActivity extends Activity {
 			addItem(scanResult);
 			etProNum.setText("");
 			etProNum.requestFocus();
+		}
+	}
+	
+	// 自定义Adapter
+	private class MyAdapter extends BaseAdapter {
+
+		private class ViewHolder {
+			public ImageButton ibDel;
+			public TextView tvProNum;
+			public TextView tvProModel;
+		}
+
+		private LayoutInflater mInflater;
+
+		public MyAdapter() {
+
+			this.mInflater = (LayoutInflater) DelRegActivity.this
+			.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		}
+
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return DelRegActivity.this.data.size();
+		}
+
+		@Override
+		public Object getItem(int arg0) {
+			// TODO Auto-generated method stub
+			return DelRegActivity.this.data.get(arg0);
+		}
+
+		@Override
+		public long getItemId(int position) {
+			// TODO Auto-generated method stub
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			// TODO Auto-generated method stub
+			ViewHolder holder;
+			if (convertView == null) {
+				convertView = mInflater.inflate(R.layout.del_item, null);
+				holder = new ViewHolder();
+				/** 得到各个控件的对象 */
+				holder.ibDel = (ImageButton) convertView
+						.findViewById(R.id.ibDel);
+				holder.tvProNum = (TextView) convertView
+						.findViewById(R.id.tvProNum);
+				holder.tvProModel = (TextView) convertView
+						.findViewById(R.id.tvProModel);
+				convertView.setTag(holder); // 绑定ViewHolder对象
+			} else {
+				holder = (ViewHolder) convertView.getTag();// 取出ViewHolder对象
+			}
+			/** 设置TextView显示的内容，即我们存放在动态数组中的数据 */
+			holder.tvProNum.setText(DelRegActivity.this.data.get(position)
+					.get("tvProNum").toString());
+			holder.tvProModel.setText(DelRegActivity.this.data.get(position)
+					.get("tvProModel").toString());
+
+			/** 为Button添加点击事件 */
+			holder.ibDel.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					System.out.println("你点击了按钮");
+				}
+			});
+
+			return convertView;
 		}
 	}
 }
